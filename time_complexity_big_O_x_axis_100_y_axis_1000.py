4ba7ba4a0152d5fe8dfa3a6b29f5a5796c97dfdc
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-11-18 08:04:54 -0800

    Create time_complexity_big_O_x_axis_100_y_axis_1000.py

---
+++ time_complexity_big_O_x_axis_100_y_axis_1000.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import gamma
+
+X = np.linspace(0, 100, 20,)
+
+plt.yticks(np.linspace(1, 1000, 20))
+plt.ylim(0, 1000)
+
+Ya = (X)
+Yb = (X**2)
+Yc = (X*np.log(X))
+Ye = (np.log(X))
+Yf = (gamma(X))
+Yg = (2**X)
+
+line1, = plt.plot(X, Ya, marker='o', label='O(n)')
+line2, = plt.plot(X, Yb, marker='o', label='O(n^2)')
+line3, = plt.plot(X, Yc, marker='o', label='O(nlog(n))')
+line5, = plt.plot(X, Ye, marker='o', label='O(log(n))')
+line6, = plt.plot(X, Yf, marker='o', label='O(n!)')
+line7, = plt.plot(X, Yg, marker='o', label='O(2^n)')
+
+plt.legend()
+plt.show()
